a9240c62feee311efbe30f49d2fccdbb7df54af4
Yossef Aita <yossefaita176@gmail.com>
2024-08-30 16:51:10 +0300

    setup project

---
+++ README.md
@@ -0,0 +1,3 @@
+# simple_games
+
+A new Flutter project.
---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SolashScreen extends StatefulWidget {
+  const SolashScreen({super.key});
+
+  @override
+  State<SolashScreen> createState() => _SolashScreenState();
+}
+
+class _SolashScreenState extends State<SolashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
